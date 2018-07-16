0025c573efb48b9390a5357ceff6b0a8e03a3f46
Rudi Loderer <loderer@hm.edu>
2018-07-16 14:16:38 +0200

    Change library naming.

--- startup.m
+++ startup.m
@@ -3,7 +3,7 @@
 % Add all MATLAB-sources to the class path. 
 addpath(genpath('C:\Users\rudi\Documents\GitHub\BaSampleApp_small'));
 % Add MATLAB-library to class path. 
-addpath('C:\Users\rudi\Documents\GitHub\BaMatlab');
+addpath('C:\Users\rudi\Documents\GitHub\jfx4matlab');
 import jfx4matlab.matlab.*;
 %--------------------------------------------------------------------------
 
